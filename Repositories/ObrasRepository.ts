20add49d3fface56a100ca82bbf2805f293e5012
Raúl Moya <rmoya889@gmail.com>
2024-01-30 18:10:23 +0100

    Changings

--- Repositories/ObrasRepository.ts
+++ Repositories/ObrasRepository.ts
@@ -1,14 +1,18 @@
 class ObrasRepository {
 
+  readonly url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
+
   /**
   * Method for get the artwork with the passed id
   *
   * params id -> id of the artwork wanted
-  *
-  * return the wanted artwork
+  * params callback -> function that specifies what happens when the function fails and when the ArtWork is correctly fetched
+  * 
   */
-  getArtWork(id: number): ArtWork {
-
-
+  getArtWork(id: number, callback: (error: Error, artwork: ArtWork) => void): void {
+    $.getJSON(this.url + id, (response) => {
+      console.log(response);
+      callback(null, response);
+    }).fail(_ => callback(new Error("Failed to fetch artwork"), null));
   }
 }
--- Services/ObrasService.ts
+++ Services/ObrasService.ts
@@ -2,29 +2,51 @@ class ObraService {
 
   private readonly LIMIT = 40;
 
-  constructor(private repository: IdobrasRepository) { };
+  constructor(private repository: ObrasRepository) { };
 
   /**
   * Method for get the artworks fit for one page
   *
   * params category -> category of the artworks
   * params offset -> position of the first element
+  * params callback -> function that specifies what happens when the function fails and when the ArtWorks are correctly fetched
   *
-  * return ArtWork[] works of the category between the offset and the constant LIMIT
   */
-  getArtWorks(category: Category, offset: number): ArtWork[] {
-
-    const artworks: ArtWork = [];
-    const array = arr.get(category);
-    for (let i = offset; i < offset + this.LIMIT; i++) {
-      let element = array[i];
-      if (typeof element === 'number') {
-        element = this.repository.getArtWork(element);
-        array[i] = element;
+  getArtWorks(department: Department, offset: number, callback: (error: Error | null, artworks: ArtWork[]) => void): void {
+    const artworks: ArtWork[] = [];
+    // const array = arr.get(department);
+    // let completed = 0;
+    //
+    // for (let i = offset; i < offset + this.LIMIT; i++) {
+    //   let element = array[i];
+    //   if (typeof element === 'number') {
+    //     this.repository.getArtWork(element, (error, artwork) => {
+    //       if (error) {
+    //         console.log(error);
+    //       } else {
+    //         artworks.push(artwork);
+    //         completed++;
+    //         if (completed === this.LIMIT) {
+    //           callback(null, artworks);
+    //         }
+    //       }
+    //     });
+    //   } else {
+    //     artworks.push(element);
+    //     completed++;
+    //     if (completed === this.LIMIT) {
+    //       callback(null, artworks);
+    //     }
+    //   }
+    // }
+    this.repository.getArtWork(1, (error, artwork) => {
+      if (error) {
+        console.log(error);
+      } else {
+        artworks.push(artwork);
+        callback(null, artworks);
       }
-      artworks.push(element);
-    }
 
-    return artworks;
+    })
   }
 }
