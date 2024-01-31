c479a84948dd2fdb6eda6e74db0d0cd26b552940
salejaldre <sergioalejaldrezarza@gmail.com>
2024-01-31 15:45:32 +0100

    Cambio models

---
+++ Models/Constituent.ts
@@ -0,0 +1,10 @@
+class Constituent {
+    constructor(
+        public constituentID: number,
+        public role: string,
+        public name: string,
+        public constituentULAN_URL: string,
+        public constituentWikidata_URL: string,
+        public gender: string
+    ) {}
+}
---
+++ Models/Tag.ts
@@ -0,0 +1,7 @@
+class Tag {
+    constructor(
+        public term: string,
+        public AAT_URL: string,
+        public Wikidata_URL: string
+    ) {}
+}
