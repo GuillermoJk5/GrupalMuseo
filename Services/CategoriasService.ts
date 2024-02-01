84de2ba9aeb975afb2a0d4e915565e28e98398e8
vvinerta <vinertacrespo@gmail.com>
2024-02-01 15:50:46 +0100

    Iniciacion del artworkMap

--- Services/CategoriasService.ts
+++ Services/CategoriasService.ts
@@ -7,8 +7,10 @@ class CategoriasService {
                 console.error("Error al realizar la llamada a la API: " + error);
                 callback(error, undefined);
             } else {
+                const departmentMap = DepartmentArtworkMap.getInstance();
                 const categories = JSON.parse(data);
                 const departments = categories.departments.map((department : any )=> new Department(department.departmentId, department.displayName));
+                departmentMap.generate(departments.map((department: { departmentId: string; }) => parseInt(department.departmentId)));
                 callback(undefined, departments);
             }
         });
