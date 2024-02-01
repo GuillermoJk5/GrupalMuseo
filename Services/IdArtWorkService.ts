1770c1c2b323e6e38afd04a626cdd43b27a436e4
salejaldre <sergioalejaldrezarza@gmail.com>
2024-02-01 16:55:41 +0100

    Change idArtworkService

--- Services/IdArtWorkService.ts
+++ Services/IdArtWorkService.ts
@@ -1,12 +1,16 @@
 class IdArtWorkService{
     getDepartmentIds(department: number, callback: (error: Error | null, data: artWork[] | null) => void): void {
+        const departmentMap = DepartmentArtworkMap.getInstance();
         const idArtWorkRepository = new IdArtWorkRepository();
+        if(departmentMap.getDepartment(department).length != 0){
+            callback(null,departmentMap.getDepartment(department));
+            return;
+        }
         idArtWorkRepository.getDepartmentIds(department, (error, data) => {
             if (error) {
                 console.error(error);
                 callback(error, []);
             } else {
-                const departmentMap = DepartmentArtworkMap.getInstance();
                 departmentMap.populate(department, data);
                 callback(null, data);
             }
