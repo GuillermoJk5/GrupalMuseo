de777ac78e83bb6470049ee77d6ec9b6a56900eb
Raúl Moya <rmoya889@gmail.com>
2024-02-05 16:33:12 +0100

    Uncomment

--- Repositories/ArtworkRepository.ts
+++ Repositories/ArtworkRepository.ts
@@ -1,18 +1,20 @@
 class ObrasRepository {
-
-  readonly url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
+  readonly url =
+    "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
 
   /**
-  * Method for get the artwork with the passed id
-  *
-  * params id -> id of the artwork wanted
-  * params callback -> function that specifies what happens when the function fails and when the ArtWork is correctly fetched
-  * 
-  */
-  getArtWork(id: number, callback: (error?: Error, artwork?: ArtWork) => void): void {
+   * Method for get the artwork with the passed id
+   *
+   * params id -> id of the artwork wanted
+   * params callback -> function that specifies what happens when the function fails and when the ArtWork is correctly fetched
+   *
+   */
+  getArtWork(
+    id: number,
+    callback: (error?: Error, artwork?: ArtWork) => void,
+  ): void {
     $.getJSON(this.url + id, (response) => {
-      console.log(response);
       callback(undefined, response);
-    }).fail(_ => callback(new Error("Failed to fetch artwork"), undefined));
+    }).fail((_) => callback(new Error("Failed to fetch artwork"), undefined));
   }
 }
